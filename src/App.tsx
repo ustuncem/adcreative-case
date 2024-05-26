c8eef8f924039c4fb7618a484f29588b14c7a194
ustuncem <ustuncem7@gmail.com>
2024-05-26 16:08:51 +0300

    chore: prepare base styles for prototyping

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div className="text-6xl font-light">TEST!</div>;
+  return (
+    <main className="h-full bg-red-400 max-w-2xl mx-auto grid place-items-center">
+      <p>CENTERED!</p>
+    </main>
+  );
 }
